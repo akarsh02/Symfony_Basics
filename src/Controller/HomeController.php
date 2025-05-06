aabc4014254605f34ee8d3386901d303839ae209
akarsh <akarsh226@gmail.com>
2025-05-06 19:38:22 +0530

    twig setup and rendering html

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,13 +2,15 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;//to load twig file
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Response;
 
-class HomeController
+class HomeController extends AbstractController
 {
  #[Route('/')]
   public function index():Response{
-   return new Response("Hello from a controller");
+   $content = $this->renderView('home/index.html.twig');
+   return new Response($content);
   }
 }
